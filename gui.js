92381635a51c4ef626e99393ed3ed7fde3b27cd9
Let'sGoPens <Let'sGoPens@LAPTOP-1RN05M2E>
2019-10-16 01:22:20 -0400

    changed font style

--- gui.js
+++ gui.js
@@ -95,7 +95,7 @@ function datGUI(){
       }
     };
 
-    gui.domElement.style.fontSize = "7pt";
+    //gui.domElement.style.fontSize = "7pt";
     let guiContainer = document.querySelector("#guiContainer");
     guiContainer.appendChild(gui.domElement);
 
@@ -115,11 +115,11 @@ function datGUI(){
     let cloudSpeedSlider = gui.add(controls,"CloudSpeed",0,50);
 
     let fModes = gui.addFolder('Modes'); //folder - then just use f1.add(...); and f1.open();
-    fModes.domElement.style.fontSize = "7pt";
+    //fModes.domElement.style.fontSize = "7pt";
     let night = fModes.add(controls, 'Night');
 
     let f1 = gui.addFolder('Display'); //folder - then just use f1.add(...); and f1.open();
-    f1.domElement.style.fontSize = "7pt";
+    //f1.domElement.style.fontSize = "7pt";
     let tint = f1.add(controls, 'Tint'); //checkbox
     let sepia = f1.add(controls, 'Sepia'); //checkbox    
     let noise = f1.add(controls, 'Noise'); //checkbox 
@@ -128,7 +128,7 @@ function datGUI(){
     let brightnessSlider = f1.add(controls, 'Brightness', 30, 100);
 
     let f2 = gui.addFolder('Effects');
-    f2.domElement.style.fontSize = "7pt";
+    //f2.domElement.style.fontSize = "7pt";
     //converting the effect checkboxes to radio buttons
     let highshelf = f2.add(controls, 'Highshelf').listen();
     let highshelfInput = [].slice.call(highshelf.domElement.childNodes);
@@ -145,7 +145,7 @@ function datGUI(){
     let reverbSlider = f2.add(controls,'ReverbValue',0,100);
 
     let f3 = gui.addFolder('Wave Distortion');
-    f3.domElement.style.fontSize = "7pt";
+    //f3.domElement.style.fontSize = "7pt";
     let distortion = f3.add(controls,'Distortion');
     let distortionSlider = f3.add(controls,'DistortionValue', 0, 100);
     

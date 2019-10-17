8ddf6b00f7edff371e598ab3992eb7abbd677d4d
Let'sGoPens <Let'sGoPens@LAPTOP-1RN05M2E>
2019-10-16 23:37:58 -0400

    removed todo items

--- loader.js
+++ loader.js
@@ -2,10 +2,3 @@ import {init} from './main.js';
 import {datGUI} from './gui.js';
 init(); //initializes everything
 datGUI(); //sets up GUI controls like dat gui and progress slider
-
-//TODO:
-//dropdown arrow
-//make sure everything works on banjo
-
-//BEFORE SUBMITTING:
-//post to banjo
